71a33c3dcc49e1a3befa0f44077657475a8a9f68
Gheorghe Isac <gheorghe_isac@smart-x.net>
2022-11-15 15:43:02 +0100

    add extra parameters for JAVA_OPTS

--- variables.tf
+++ variables.tf
@@ -135,6 +135,12 @@ variable "time_limit_ms" {
   type        = number
 }
 
+variable "java_opts" {
+  description = "Custom Java Options"
+  default     = ""
+  type        = string
+}
+
 # --- Telemetry
 
 variable "telemetry_enabled" {
